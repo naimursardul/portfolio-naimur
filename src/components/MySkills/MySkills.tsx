b4dc8aa566105f5fb0fb9bba8f13dac567614c4a
Naimur <naimur30447@gmail.com>
2024-06-12 07:17:52 +0600

    init

--- src/components/MySkills/MySkills.tsx
+++ src/components/MySkills/MySkills.tsx
@@ -1,22 +1,55 @@
 import { IoLogoJavascript } from "react-icons/io5";
-import { FaCss3Alt } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiExpress, SiMongodb, SiNextdotjs } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import NodeJsIcon from "../../../public/node-js.png";
+import NextJsIcon from "../../../public/nextjs.jpeg";
+import Image from "next/image";
 
 const skills: any = [
   {
     name: "JavaScript",
-    icon: <IoLogoJavascript />,
-    color: "yellow",
+    icon: <IoLogoJavascript className="text-[yellow]" />,
   },
   {
     name: "HTML",
-    icon: <FaHtml5 />,
-    color: "#DE4B25",
+    icon: <FaHtml5 className="text-[#DE4B25]" />,
   },
   {
     name: "CSS",
-    icon: <FaCss3Alt />,
-    color: "#0B70B2",
+    icon: <FaCss3Alt className="text-[#0B70B2]" />,
+  },
+  {
+    name: "Node.js",
+    icon: <Image src={NodeJsIcon} alt="" height={50} width={50} />,
+  },
+  {
+    name: "Express.js",
+    icon: <SiExpress className="text-[black] bg-white rounded-full p-2" />,
+  },
+  {
+    name: "MongoDB",
+    icon: <SiMongodb className="text-[#00ED64] bg-black p-2 rounded-full " />,
+  },
+  {
+    name: "React",
+    icon: <FaReact className="text-[#5ED3F3] " />,
+  },
+  {
+    name: "Next.js",
+    icon: (
+      <Image
+        src={NextJsIcon}
+        alt=""
+        width={50}
+        height={50}
+        className="rounded-full"
+      />
+    ),
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <RiTailwindCssFill className="text-[#5AC9F9] " />,
   },
 ];
 
@@ -24,15 +57,13 @@ export default function MySkills() {
   return (
     <div className="flex flex-col items-center gap-8 globPad bg-[--bgSoft]">
       <h2 className="text-5xl font-[700] text-[--text]">My Skills</h2>
-      <div className="flex flex-wrap gap-8">
+      <div className="flex flex-wrap justify-center gap-8">
         {skills.map((skill: any, i: any) => (
           <div
             key={i}
-            className="w-[150px] h-[150px] flex flex-col items-center justify-center gap-4 bg-[--bgSofter] rounded-lg"
+            className="w-[200px] max-md:w-[120px] h-[200px] max-md:h-[120px] flex flex-col items-center justify-center gap-4 bg-[--bgSofter] rounded-lg"
           >
-            <div className={`text-5xl text-[${skill?.color}]`}>
-              {skill?.icon}
-            </div>
+            <div className="text-5xl">{skill?.icon}</div>
             <p className="text-xl text-[--text] font-[500] ">{skill?.name}</p>
           </div>
         ))}
